d3000132970da6cc2ff2fbcb65c01d15e972709d
Carlos Diaz <cdiaz86@gmail.com>
2020-08-14 15:56:27 -0400

    Adds counter view.

--- src/context/counter-context.js
+++ src/context/counter-context.js
@@ -2,7 +2,6 @@ import React, { useState, createContext } from "react";
 
 // creates context object
 export const CounterContext = createContext();
-console.log(CounterContext);
 
 // create provider for components to consume context and subscribe to changes
 export const CounterContextProvider = props => {
